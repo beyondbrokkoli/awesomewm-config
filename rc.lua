27e19c2e0df4997bde20fd51301797059eaaaf93
Halim Zaghdoudi <halim.zag@gmx.de>
2026-02-24 12:25:43 +0100

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -138,7 +138,7 @@ clientbuttons = gears.table.join(
 
 root.keys(globalkeys)
 awful.rules.rules = {
-    { rule = { }, properties = { border_width = beautiful.border_width, border_color = beautiful.border_normal, focus = awful.client.focus.filter, raise = true, keys = clientkeys, buttons = clientbuttons, screen = awful.screen.preferred, placement = awful.placement.no_overlap+awful.placement.no_offscreen } },
+    { rule = { }, properties = { border_width = 0, border_color = beautiful.border_normal, focus = awful.client.focus.filter, raise = true, keys = clientkeys, buttons = clientbuttons, screen = awful.screen.preferred, placement = awful.placement.no_overlap+awful.placement.no_offscreen } },
     { rule_any = { instance = { "DTA", "copyq", "pinentry" }, class = { "Arandr", "Blueman-manager", "Gpick", "Kruler", "Sxiv", "Wpa_gui", "veromix" } }, properties = { floating = true } },
     { rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = false } }
 }
